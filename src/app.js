b3b0a0d50a0d7b40188bc243a67f55480e45d7b9
mikezcode <tamirumolla24@gmail.com>
2021-09-17 01:56:28 +1200

    update

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const hbs = require("hbs")
 const path = require("path")
 const geocode = require("./utils/geocode")
-const forcast = require("./ utils/forcast")
+const forcast = require("./utils/forcast")
 
 const app =express();
 const port = process.env.PORT || 3000
